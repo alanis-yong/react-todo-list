8ba0c8e7420b19c5e529e0e783844f100b16e6bf
alanis-yong <alanisyongyr@gmail.com>
2026-02-03 22:06:52 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,47 @@
 import { useContext } from 'react';
-import { Badge, Card, Col, Container, Row } from 'react-bootstrap';
+import { Badge, Card, Col, Container, Row, Button } from 'react-bootstrap'; 
 import { TodoContext } from '../contexts/TodoContext';
 
 export default function Home() {
-  const todos = useContext(TodoContext).todos;
+  const { todos, setTodos } = useContext(TodoContext);
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <Container>
       <h1 className="my-3">Your todos</h1>
       <Row>
-        <CardGroup todos={todos} />
+        <CardGroup todos={todos} deleteTodo={deleteTodo} />
       </Row>
     </Container>
   );
 }
 
-function CardGroup({ todos }) {
-  return todos.map((todos) => {
-    const completed = todos.completed;
+function CardGroup({ todos, deleteTodo }) {
+  return todos.map((todo) => {
+    const completed = todo.completed;
     const bg = completed ? 'success' : 'danger';
     return (
-      <Col md={4} key={todos.id}>
+      <Col md={4} key={todo.id}>
         <Card className="my-3">
           <Card.Body>
-            <Card.Title>{todos.title}</Card.Title>
-            <Card.Text>{todos.description}</Card.Text>
+            <Card.Title>{todo.title}</Card.Title>
+            <Card.Text>{todo.description}</Card.Text>
             <Badge bg={bg}>{!completed && 'Not'} Completed</Badge>
+             <div className="mt-3">
+              <Button 
+                variant="outline-danger" 
+                size="sm" 
+                onClick={() => deleteTodo(todo.id)}
+              >
+                Delete
+              </Button>
+            </div>
           </Card.Body>
         </Card>
       </Col>
     );
   });
 }
- 
